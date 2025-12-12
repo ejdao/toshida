cd50132615533b5c1316cc1cef842bf23e6480d7
ejdao <ejdao2022@gmail.com>
2025-12-12 08:33:39 -0500

    :fire: Testing removido

--- libraries/components/testing/component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  standalone: true,
-  selector: 'tsd-testing',
-  templateUrl: './component.html',
-  styleUrl: './component.scss',
-})
-export class TestingComponent {}
